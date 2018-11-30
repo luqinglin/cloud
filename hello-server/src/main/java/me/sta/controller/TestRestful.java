fd00f02b1d5fb8c7f4615e5058d0d7393c12b15e
luqinglin <luqinglin@iqianbang.com>
2018-11-30 16:45:59 +0800

    配置中心

--- hello-server/src/main/java/me/sta/controller/TestRestful.java
+++ hello-server/src/main/java/me/sta/controller/TestRestful.java
@@ -1,17 +1,20 @@
 package me.sta.controller;
 
 import me.sta.helloservice.ServiceApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestRestful implements ServiceApi {
 
+    @Value("${form}")
+    private String form;
 
     @Override
     public String home(String username,String passwd){
         System.out.println(username);
         System.out.println(passwd);
-        String s = "{\"id\":1}";
+        String s = "{\"id\":"+form+"}";
         return s;
     }
     @Override
