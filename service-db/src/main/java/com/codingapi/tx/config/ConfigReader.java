79b940e4b03b98627c72b17f08170a04211b47d2
luqinglin <luqinglin@iqianbang.com>
2019-07-02 08:28:14 +0800

    分布式事务插件可插拔

--- service-db/src/main/java/com/codingapi/tx/config/ConfigReader.java
+++ service-db/src/main/java/com/codingapi/tx/config/ConfigReader.java
@@ -1,6 +1,7 @@
 package com.codingapi.tx.config;
 
 import com.codingapi.tx.config.service.TxManagerTxUrlService;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +28,9 @@ public class ConfigReader {
 
 
     public String getTxUrl() {
+        if (StringUtils.isEmpty(url)){
+            return null;
+        }
 
         try {
             txManagerTxUrlService =  spring.getBean(TxManagerTxUrlService.class);
